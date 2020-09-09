ae7f3ce46dd0d5669002c7ab115374c74578d1f5
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-09-09 16:34:17 +0100

    set background for first page

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonGrid, IonRow, IonCol, IonButton } from '@ionic/react';
 // import { IonMenu,  IonList, IonItem, IonRouterOutlet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
@@ -9,16 +9,21 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Photified</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Photified</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 10 page" />
+          <IonGrid>
+              <IonRow>
+                  <IonCol size="12"> <IonButton href="#">Get Started</IonButton></IonCol>
+              </IonRow>
+          </IonGrid>
+        {/*<ExploreContainer name="Tab 10 page" />*/}
       </IonContent>
     </IonPage>
   );
